93e49f6b637ae296f64fda02440ee31ab11af58d
Gaurav Bansal <bansal22.gaurav@gmail.com>
2022-06-09 16:55:28 -0700

    heheha

--- main.py
+++ main.py
@@ -2,9 +2,34 @@
 
 keys = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N", "O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N", "O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+lowalphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n", "o","p","q","r","s","t","u","v","w","x","y","z"]
 
 for i in range(26):
     x = input("What do you want the key for " + alphabet[i] + " to be?")
     keys[i] = x
 
-print(keys)
+def loopMain():   
+    ask = input("To encrypt a message write E.")
+    if ask == "E":
+        msg = input("Write your message: ")
+        chars = list(msg)
+        enc = ""
+        for char in chars:
+            cap = char
+            if char in alphabet:
+                num = alphabet.index(char)
+                enc += keys[num]
+            elif char in lowalphabet:
+                num = lowalphabet.index(char)
+                enc += keys[num]
+            else:
+                enc += char
+        
+        print(enc)
+    else:
+        loopMain()   
+
+while True:
+    loopMain()
+
+
